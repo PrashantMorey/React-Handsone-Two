b3e77559971ae002c8b2574c35401ca3810aff96
Prashant Morey <prashantmorey88308@gmail.com>
2023-05-03 19:18:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React ,{ useState } from 'react';
 import './App.css';
 
 function App() {
+  //FormData => a variable
+  //setFormData => a function
+  // useState => an array of length 2
+  // DRY Principle => Do not Repeat yourself
+  const [allFormData, setAllFormData] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    department: "",
+    rating: "",
+  });
+
+  // event => object that repersent the event
+  const handelChange = (event) => {
+    setFormData({...formData,[event.target.name]: event.target.value});
+  }
+  const handleDisabled = () => {
+    if(!formData.name || !formData.department || !formData.rating){
+      return true;
+    }
+    return false;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EMPLOYEE FEEDBACK FORM </h1>
+      <form>
+        <label>
+          Name :
+          <input type="text" name='name' onChange={handelChange}/>
+        </label>
+        <label>
+          Department :
+          <input type="text" name='department' onChange={handelChange}/>
+        </label>
+        <label>
+          Rating :
+          <input type="number" name='rating' onChange={handelChange}/>
+        </label>   
+        <button type='button' disabled={handleDisabled()} onClick={(e) => {
+          e.preventDefault();
+          const tempobj = [...allFormData];
+          tempobj.push(formData);
+          setAllFormData(tempobj);
+          // line no. 38 & 39
+          // setAllFormData([...allFormData, formData]);
+          console.log(formData);
+        }}>Submit</button>            
+      </form>
+      <div className="mainEle"> 
+      {allFormData.map((value,index)=>{
+        return <div className='returnEle' key={index}>
+          Name: {value.name} | 
+          Department: {value.department} | 
+          Rating: {value.rating}
+          </div>;
+      })}
     </div>
+    </div>
+    
   );
 }
 
